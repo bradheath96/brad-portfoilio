3473a2e94278f0fcfab0a2b28f0ad94b40a3bf1a
Brad Heath <bradheath96@gmail.com>
2024-09-04 10:33:53 +0100

    added name animations to the tech grid

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+	const menu = document.querySelector(".menu-links");
+	const icon = document.querySelector(".hamburger-icon");
+	menu.classList.toggle("open");
+	icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	const techItems = document.querySelectorAll(".tech-item");
+
+	techItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			techItems.forEach((i) => i.classList.remove("show"));
+			item.classList.add("show");
+		});
+	});
+});
