3830f614cf007f27df9527d02e83329b5e245280
Brad Heath <bradheath96@gmail.com>
2024-09-06 14:56:27 +0100

    start dark and light mode features

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	});
 });
+
+// dark/light mode
+
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+
+document.addEventListener("DOMContentLoaded", () => {
+	const savedTheme = localStorage.getItem("theme");
+	if (savedTheme === "dark") {
+		setDarkMode();
+	} else {
+		setlightMode();
+	}
+});
+
+btn.addEventListener("click", function () {
+	setTheme();
+});
+
+btn2.addEventListener("click", function () {
+	setTheme();
+});
+
+function setTheme() {
+	let currentTheme = document.body.getAttribute("theme");
+	if (currentTheme === "dark") {
+		setlightMode();
+	} else {
+		setDarkMode();
+	}
+}
+
+function setDarkMode() {
+	document.body.setAttribute("theme", "dark");
+	localStorage.setItem("theme", "dark");
+	themeIcons.forEach((icon) => {
+		icon.src = icon.getAttribute("src-dark");
+	});
+}
+
+function setlightMode() {
+	document.body.setAttribute("theme", "light");
+	localStorage.setItem("theme", "light");
+	themeIcons.forEach((icon) => {
+		icon.src = icon.getAttribute("src-light");
+	});
+}
